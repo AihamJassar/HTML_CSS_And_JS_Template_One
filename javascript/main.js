8901d676b07251e2907bae6362df46d92d80ff98
Aiham Jassar <aihamjassar@gmail.com>
2025-03-25 03:54:20 +0300

    add new images and implement gallery section with popup functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,21 @@
 let landing = document.getElementById("landing");
 let landingBackground = [
-  "../images/landing-1.jpg",
-  "../images/landing-2.jpg",
-  "../images/landing-3.jpg",
+  "../images/01.jpg",
+  "../images/02.jpg",
+  "../images/03.jpg",
+  "../images/04.jpg",
+  "../images/05.jpg",
 ];
 let index = 1;
 
-function changeBackground(){
+function changeBackground() {
   return window.setInterval((_) => {
-  if (index === landingBackground.length) {
-    index = 0;
-  }
-  landing.style.backgroundImage = `url(${landingBackground[index]})`;
-  index++;
-}, 100000);
+    if (index === landingBackground.length) {
+      index = 0;
+    }
+    landing.style.backgroundImage = `url(${landingBackground[index]})`;
+    index++;
+  }, 100000);
 }
 changeBackground();
 let bars = document.getElementById("bars");
@@ -41,7 +43,9 @@ colors.addEventListener("click", (event) => {
   let mainColor = event.target.dataset.color;
   if (mainColor) {
     document.documentElement.style.setProperty(`--main-color`, mainColor);
-    document.querySelectorAll(".settings-box .box .colors li.main-color").forEach((li) => li.classList.remove("main-color"));
+    document
+      .querySelectorAll(".settings-box .box .colors li.main-color")
+      .forEach((li) => li.classList.remove("main-color"));
     event.target.classList.add("main-color");
     localStorage.setItem("main-color", mainColor);
   }
@@ -66,14 +70,49 @@ if (chBa === "no change") {
   changedBackground.removeAttribute("checked");
   clearInterval(changeBackground);
 }
-console.log("Checkbox : " + changedBackground.getAttribute("checked"))
-changedBackground.addEventListener("click", event => {
-  if(!event.target.getAttribute("checked")){
-    
-    clearInterval(chandeBackground());
+console.log("Checkbox : " + changedBackground.getAttribute("checked"));
+changedBackground.addEventListener("click", (event) => {
+  if (!event.target.getAttribute("checked")) {
+    clearInterval(changeBackground());
     localStorage.setItem("changedBackground", "no change");
   } else {
     localStorage.setItem("changedBackground", "change");
     changeBackground();
   }
+});
+
+let skillsSection = document.getElementById("skills");
+window.onscroll = function () {
+  let offsetTop = skillsSection.offsetTop;
+  let offsetHeight = skillsSection.offsetHeight;
+  let innerHeight = window.innerHeight;
+  let windowScrollTop = window.scrollY;
+  if (windowScrollTop > offsetHeight + offsetTop - innerHeight) {
+    let skills = document.querySelectorAll(".skills .box .skill-progress span");
+    skills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
+
+let gallery = document.querySelectorAll(".gallery .images-box img");
+gallery.forEach((img) => {
+  img.addEventListener("click", (_) => {
+    let overlay = document.createElement("div");
+    overlay.className = "overlay";
+    document.body.appendChild(overlay);
+    let popup = document.createElement("div");
+    popup.className = "popup";
+    if (img.alt !== null) {
+      let title = document.createElement("h3");
+      let titleText = document.createTextNode(img.alt);
+      title.appendChild(titleText);
+      popup.appendChild(title);
+    }
+    let image = document.createElement("img");
+    image.src = img.src;
+    console.log(img);
+    popup.appendChild(image);
+    document.body.appendChild(popup);
+  });
 });
