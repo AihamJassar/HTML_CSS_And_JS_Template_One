bde7cc7feb9bafc15c77091d172da04192802c65
AihamJassar <aihamjassar@gmail.com>
2025-03-26 04:40:35 +0300

    update

--- javascript/main.js
+++ javascript/main.js
@@ -82,7 +82,7 @@ changedBackground.addEventListener("click", (event) => {
 });
 
 let skillsSection = document.getElementById("skills");
-window.onscroll = function () {
+window.onscroll = function() {
   let offsetTop = skillsSection.offsetTop;
   let offsetHeight = skillsSection.offsetHeight;
   let innerHeight = window.innerHeight;
@@ -99,20 +99,64 @@ let gallery = document.querySelectorAll(".gallery .images-box img");
 gallery.forEach((img) => {
   img.addEventListener("click", (_) => {
     let overlay = document.createElement("div");
-    overlay.className = "overlay";
+    overlay.className = "popup-overlay";
     document.body.appendChild(overlay);
     let popup = document.createElement("div");
     popup.className = "popup";
     if (img.alt !== null) {
       let title = document.createElement("h3");
+      title.className = "popup-title"
       let titleText = document.createTextNode(img.alt);
       title.appendChild(titleText);
       popup.appendChild(title);
     }
+    let closeButton = document.createElement("span");
+    let closeText = document.createTextNode("x");
+    closeButton.appendChild(closeText);
+    closeButton.className = "close-button";
+    popup.appendChild(closeButton);
+    
     let image = document.createElement("img");
     image.src = img.src;
-    console.log(img);
     popup.appendChild(image);
     document.body.appendChild(popup);
   });
 });
+
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("close-button")) {
+    document.querySelector(".popup-overlay").remove();
+    e.target.parentNode.remove();
+  }
+});
+
+let nav = document.querySelector(".nav");
+nav.addEventListener("click", e => {
+  const sectionId = e.target.dataset.section;
+  if (sectionId) {
+    document.getElementById(sectionId).scrollIntoView();
+  }
+});
+
+let bulletsOption = document.querySelectorAll(".settings-box .box .bullets span");
+let bulletsOpStorage = localStorage.getItem("bullets-option");
+if (bulletsOpStorage === "none") {
+  nav.style.display = "none";
+  bulletsOption[0].classList.remove("active");
+  bulletsOption[1].classList.add("active");
+}
+bulletsOption.forEach(el => {
+  el.addEventListener("click", e => {
+    nav.style.display = e.target.dataset.option;
+    bulletsOption.forEach(el => {
+      el.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    localStorage.setItem("bullets-option", e.target.dataset.option);
+  })
+});
+
+document.querySelector(".reset").onclick = _ => {
+  localStorage.clear();
+  location.reload();
+}
